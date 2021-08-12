18a7caa05bae742552e18012b5462dfe47fe197a
Shreyasaraf28 <shreysaraf98@gmail.com>
2021-08-12 17:04:37 +0530

    comit 1

---
+++ models/student.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+
+const studentSchema = new mongoose.Schema({
+
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    subject: {
+        type: String,
+        required: true
+    }
+
+})
+
+module.exports = mongoose.model('Student',studentSchema)
